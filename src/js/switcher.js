98023852d87c87c79d71fcaa89e7f444519071d8
AleksandrNebesnyi <82997944+AleksandrNebesnyi@users.noreply.github.com>
2021-10-07 11:38:51 +0300

    Initial commit

---
+++ src/js/switcher.js
@@ -0,0 +1,61 @@
+// Импортируем элементы меню, шаблон из templates
+// import '../sass/main.scss';
+// import cardTemplate from '../templates/menus-card.hbs';
+import menuItems from '../menu.json';
+import cardTemplates from '../templates/menu-cards';
+// console.log(cardTemplates);
+
+// Выбираем элементи из HTML.
+const menuList = document.querySelector('.js-menu');
+const switchCheckBox = document.querySelector('#theme-switch-toggle');
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+// Функция создания разметки на основе шаблона.
+
+function createMenuCardMarkup(menuItems) {
+  //   return menuItems.map(menuItems => cardTemplate(menuItems)).join('');
+  // return menuItems.map(cardTemplate).join('');
+  return cardTemplates(menuItems);
+}
+// Генерируем все элементы
+const menuMarkup = createMenuCardMarkup(menuItems);
+
+// Вставляем элементы в разметкую
+menuList.insertAdjacentHTML('beforeend', menuMarkup);
+
+// Слушатель на переключатель
+switchCheckBox.addEventListener('change', event);
+
+function event() {
+  if (switchCheckBox.checked) {
+    document.body.classList.add(Theme.DARK);
+  } else {
+    document.body.classList.replace(Theme.DARK, Theme.LIGHT);
+  }
+  //Сохраняем выбранную тему
+  // Свойство classList возвращает псевдомассив DOMTokenList, содержащий
+  // все классы элемента.
+  localStorage.setItem(
+    'theme',
+    JSON.stringify({ theme: document.body.classList[1], checked: switchCheckBox.checked }),
+  );
+}
+
+// Функция применения сохраненной темы при перезагрузке
+
+const SetThemeAfterReload = function () {
+  const saveTheme = localStorage.getItem('theme');
+  if (saveTheme) {
+    const { theme, checked } = JSON.parse(saveTheme);
+
+    document.body.classList.add(theme);
+    switchCheckBox.checked = checked;
+  } else {
+    document.body.classList.add(Theme.LIGHT);
+  }
+};
+SetThemeAfterReload();
